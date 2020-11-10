f66aa6ab86d0dd97e1635b32cd0126ebade1426c
wujunjie021513 <30453050+wujunjie021513@users.noreply.github.com>
2020-11-10 22:46:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "茄生日快乐~": "./imgs/qie.jpg",
+        "茄生日快乐": "./imgs/qie.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -35,8 +35,8 @@ var config = {
         play: "音乐",
         bannar_coming: "彩带",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        cake_fadein: "来个蛋糕",
+        light_candle: "来只蜡烛",
         wish_message: "生日快乐!",
         story: "A MESSAGE FOR YOU",
     }
